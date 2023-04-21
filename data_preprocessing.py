54e53972451277a9a38c1d1bee4535fe18df66a4
adminuser <adminuser@kali.mvm>
2023-04-21 19:05:47 +0530

    edited data preprocessing file

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -3,10 +3,35 @@ import numpy as np
 import matplotlib.pyplot as plt 
 
 class DataPreprocessor: 
-    def __init__(self): 
-        pass 
+    def __init__(self, roberta_preprocessor_model, roberta_input_features): 
+        """
+            this class provides the methods, that needed for the preprocessing of the Entailment dataset.
+            Methods:
+                dataframe_to_tensor_dataset
+                reshape_image
+                get_encoder_input
+                preprocess_text_and_image
+                prepare_dataset
+            
+            Attrs:
+                roberta_preprocessor_model(type: keras.models.Model): roberta preprocessor model, which is need by
+                                                 get_encoder_input method
+                roberta_input_features(type: List): list of names of the input features of the roberta model.
+
+        """
+        self.roberta_preprocessor_model = roberta_preprocessor_model
+        self.roberta_input_features = roberta_input_features
 
     def dataframe_to_tensor_dataset(self, dataframe):
+        """
+            this method will convert the pandas dataframe into the tensorflow dataset, for the better processing of the tpu or 
+            gpu
+            Params:
+                dataframe(type: pandas.DataFrame): pandas dataframe, that needed to be converted.
+
+            Return(type: tf.data.Dataset):
+                tensorflow dataset, for the better performance.
+        """
         columns = ["image_1_path", "image_2_path", "text_1", "text_2", "label_idx"]
         dataframe = dataframe[columns].copy()
         labels = dataframe.pop("label_idx")
@@ -14,7 +39,18 @@ class DataPreprocessor:
         ds = ds.shuffle(buffer_size=len(dataframe))
         return ds
 
-    def reshape_image(image_path, img_shape):
+    def reshape_image(self, image_path, img_shape):
+        """
+            this method, will read the image data from the image path, and reshape the image into the specified image
+            shape.
+            Params:
+                image_path(type: str): Image Directory path for the tweet images.
+                img_shape(type: tuple): Image shape, that the image needed to be resized.
+
+            Return(type; np.ndarray):
+                Numpy N-dimensional array.(image)
+
+        """
         extension = tf.strings.split(image_path)[-1]
 
         image = tf.io.read_file(image_path)
@@ -25,23 +61,60 @@ class DataPreprocessor:
         image = tf.image.resize(image, img_shape)
         return image
 
-    def get_encoder_input(roberta_preprocessor_model, text_1, text_2, roberta_input_features):
+    def get_encoder_input(self.text_1, text_2):
+                                                            
+        """
+            this method, will input the data into roberta preprocessor model(bert model), and gets the embedding of the
+            sentence.
+            Params:
+                text_1(type: str): text_1, the textual data that needed to converted as a embedding.
+                text_2(type: str): text_2, the textual data that needed to converted as a embedding.
+            
+            Return(type: List)
+                It returns the roberta preprocessed models output, which is the input of the roberta encoder model.
+        """
         text_1 = tf.convert_to_tensor([text_1])
         text_2 = tf.convert_to_tensor([text_2])
-        encoder_inputs = roberta_preprocessor_model([text_1, text_2])
-        encoder_inputs = {feature: tf.squeeze(encoder_inputs[feature]) for feature in roberta_input_features}
+        encoder_inputs = self.roberta_preprocessor_model([text_1, text_2])
+        encoder_inputs = {feature: tf.squeeze(encoder_inputs[feature]) for feature in self.roberta_input_features}
         return encoder_inputs
 
-    def preprocess_text_and_image(sample, roberta_preprocessor_model, roberta_input_features, img_shape):
+    def preprocess_text_and_image(self, sample, img_shape):
+        """
+            this method will read the image convert the input image into specific image shape with the help of reshape_image 
+            method. And also , it will get the embedding of the text1 and text2 in the dataframe.
+            Params:
+                sample(type: tf.data.Dataset): sample is the col of the tf dataset.
+                img_shape(type: tuple): Image, input shape.
+            
+            Return(type: dict)
+                this method, returns the dict of preprocessed image_1 and image_2 and encoder_input(out of the roberta preprocessor)
+        """
         image_1 = self.reshape_image(sample["image_1_path"], img_shape)
         image_2 = self.reshape_image(sample["image_2_path"], img_shape)
-        encoder_inputs = self.get_encoder_input(roberta_preprocessor_model, sample["text_1"], sample["text_2"], roberta_input_features)
+        encoder_inputs = self.get_encoder_input(self.roberta_preprocessor_model, sample["text_1"], 
+                                                            sample["text_2"], self.roberta_input_features)
         return {"image_1": image_1, "image_2": image_2, "text": encoder_inputs}
 
-    def prepare_dataset(dataframe, batch_size, auto, training=True):
+    def prepare_dataset(self, dataframe, batch_size, auto, training=True):
+        """
+            this method, will make sure to edit the tensorflow data datasets data, before passing it into the model. Which will
+            convert the image_path into actual image data and textual data into embeddings.
+            Params:
+                roberta_preprocessor_model(type: keras.models.Model): roberta preprocessor model, which is need by
+                                                 get_encoder_input method
+                roberta_input_features(type: List): list of names of the input features of the roberta model.
+                dataframe(type: pandas.DataFrame): DataFrame, that needed to preprocssed and converted into the dataset(tensorflow)
+                batch_size(type: int): Batch Size.
+                auto: prefetch auto type.
+                training(type: bool): Specifying whether it is training or testing data.
+
+            Return(type: tf.data.Dataset)
+                this method returns the tensorflow dataset object after doing all the preprocessing works.
+        """
         ds = self.dataframe_to_tensor_dataset(dataframe)
         if training:
             ds = ds.shuffle(len(train_df))
-        ds = ds.map(lambda x, y: (self.preprocess_text_and_image(x, roberta_preprocessor_model, ROBERTA_INPUT_FEATURES, IMG_SHAPE), y)).cache()
+        ds = ds.map(lambda x, y: (self.preprocess_text_and_image(x, IMG_SHAPE), y)).cache()
         ds = ds.batch(batch_size).prefetch(auto)
         return ds
--- train.py
+++ train.py
@@ -9,10 +9,12 @@ import argparse
 from data_preprocessor import *
 from utils import *
 import warnings, os
+import pickle 
+
 
 warnings.filterwarnings('ignore')
-parser = argparse.ArgumentParser()
 
+parser = argparse.ArgumentParser()
 parser.add_argument('--image-shape', description="Image Shape, to which images needed to be resized", default=(128, 128))      
 parser.add_argument('--input-dims', description="Input dims of the image to model.", default=(128, 128, 3))      
 parser.add_argument('--batch-size', description="Batch Size for the training data.", default=32)      
@@ -20,7 +22,9 @@ parser.add_argument('--save-path', description="Path to save the trained multimo
 parser.add_argument('--save-model', description="Save the trained model", default=True)  
 parser.add_argument('--image-dir', description="Directory path of the tweets image data", default="tweets_images/")      
 parser.add_argument('--epoch', description="Number of epochs to train the model", default=10)      
-   
+parser.add_argument('--lr', description="Learning Rate for the optimizer", default=0.001)      
+parser.add_argument('--opt-type', description="Optimizer for building the model, options are (sgd, adam)", default="adam")      
+args = parser.parse_args()
 
 if args.image_shape != args.(input_dims[0],args.input_dims[1]):
     raise ValueError("Image Shape and Input Dims shape should be same")
@@ -30,6 +34,9 @@ if not os.path.exists(args.image_dir):
 
 if args.batch_size > 2000:
     raise ValueError("Batch Size should be lesser than <2000")
+
+if args.opt_type not in ["adam", "sgd"]:
+    raise ValueError("Specified optimizer is not supported")
 
 
 ROBERTA_PREPROCESSOR_PATH = "https://tfhub.dev/jeongukjae/roberta_en_cased_preprocess/1"
@@ -42,53 +49,67 @@ BATCH_SIZE  = args.batch_size
 AUTO =  tf.data.AUTOTUNE
 INPUT_DIMS = args.input_dims
 
-m_builder = ModelBuilder()
-data_preprocessor = DataPreprocessor()
-roberta_preprocessor_model = build_roberta_preprocessor(ROBERTA_PREPROCESSOR_PATH)
 
-df = pd.read_csv(dataframe_file+"/tweets.csv")
-create_images_path(df)
-train_df, test_df = train_test_split(
-    df, test_size=0.1, stratify=df["label"].values, random_state=42
-)
-
-train_ds = data_preprocessor.prepare_dataset(train_df, BATCH_SIZE, AUTO)
-test_ds = data_preprocessor.prepare_dataset(test_df,BATCH_SIZE, AUTO, False)
-
-multimodal_model = m_builder.create_multimodal_model(ROBERTA_ENCODER_PATH, INPUT_DIMS, ROBERTA_INPUT_FEATURES)                                            
-train_labels = get_labels(train_ds)
-test_labels = get_labels(test_ds)
-
-def train_model(multimodal_model, lr, optimizer, train_labels, train_ds, test_ds): 
+def train_model(multimodal_model, train_labels, train_ds, val_ds): 
     
     """
         Main Function to train the MultiModality Model.
         Params:
             multimodal_model(type: keras.models.Model): Build and uncompiled keras model.
-            lr(type: float): learning Rate for the optimizer.
-            optimizer(type: str): Optimzer name, to be used.
             train_labels(type: np.array): Classes in the dataset.
             train_ds(type: tf.data.Dataset): Training dataset of the model.
-            test_ds(type: tf.data.Dataset): Testing dataset of the model.
+            val_ds(type: tf.data.Dataset): vaidation dataset of the model.
 
     """
-    train_labels_ohe = keras.utils.to_categorical(train_labels)
-    class_totals = train_labels_ohe.sum(axis=0)
-    class_weight = dict()
+    try: 
+        train_labels_ohe = keras.utils.to_categorical(train_labels)
+        class_totals = train_labels_ohe.sum(axis=0)
+        class_weight = dict()
 
-    for i in range(0, len(class_totals)):
-        class_weight[i] = class_totals.max() / class_totals[i]
-    
-    opt = get_optimizer(lr, optimizer)
-    
-    multimodal_model.compile(optimizer=opt, loss="sparse_categorical_crossentropy", 
-                                             metrics="accuracy")
-    multimodal_model.fit(train_ds, epochs=epochs, 
-                                    validation_data=test_ds, batch_size=batch_size, 
-                                    class_weight=class_weight)
+        for i in range(0, len(class_totals)):
+            class_weight[i] = class_totals.max() / class_totals[i]
+        
+        opt = get_optimizer(lr, optimizer)
+        
+        multimodal_model.compile(optimizer=opt, loss="sparse_categorical_crossentropy", 
+                                                metrics="accuracy")
+        multimodal_model.fit(train_ds, epochs=epochs, 
+                                        validation_data=val_ds, batch_size=batch_size, 
+                                        class_weight=class_weight)
 
-    if args.save_model and args.save_path: 
-        multimodal_model.save(save_path)
+        if args.save_model and args.save_path: 
+            multimodal_model.save(save_path)
+
+    except Exception as error: 
+        return error
 
 if __name__ == "-_main_ _": 
-    train_model(multimodal_model, 15, 0.0001, 32, "adam", train_labels)
+
+    model_builder = ModelBuilder()
+    roberta_preprocessor_model = build_roberta_preprocessor(ROBERTA_PREPROCESSOR_PATH)
+    data_preprocessor = DataPreprocessor(roberta_preprocessor_model, ROBERTA_INPUT_FEATURES)
+
+    df = pd.read_csv(dataframe_file+"/tweets.csv")
+    create_images_path(df)
+
+    train_df, val_df = train_test_split(
+        df, test_size=0.1, stratify=df["label"].values, random_state=42
+    )
+
+    train_df, test_df = train_test_split(
+        train_df, test_size=0.1, stratify=train_df["label"].values, random_state=42
+    )
+
+    with open('test_df.obj', "wb") as f: 
+        pickle.dump(test_df, f)
+
+    train_ds = data_preprocessor.prepare_dataset(train_df, BATCH_SIZE, AUTO)
+    test_ds = data_preprocessor.prepare_dataset(test_df, BATCH_SIZE, AUTO, False)
+    val_ds = data_preprocessor.prepare_dataset(val_df, BATCH_SIZE, AUTO, False)
+
+    multimodal_model = model_builder.create_multimodal_model(ROBERTA_ENCODER_PATH, INPUT_DIMS, ROBERTA_INPUT_FEATURES)                                            
+    train_labels = get_labels(train_ds)
+    test_labels = get_labels(test_ds)
+    val_labels = get_labels(val_ds)
+
+    train_model(multimodal_model, train_labels, train_ds, val_ds)
