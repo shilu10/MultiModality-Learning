24f0ba901884e39b1d29d93f6655e6e95b2277a8
adminuser <adminuser@kali.mvm>
2023-04-24 20:06:46 +0530

    updated code and readme

---
+++ download_dataset.sh
@@ -0,0 +1,3 @@
+wget "https://github.com/sayakpaul/Multimodal-Entailment-Baseline/raw/main/csvs/tweets.csv" dataset/ --quiet
+wget  "https://github.com/sayakpaul/Multimodal-Entailment-Baseline/releases/download/v1.0.0/tweet_images.tar.gz" dataset/ --quiet 
+tar -xzf tweet_images.tar.gz"
